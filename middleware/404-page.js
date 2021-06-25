89d3218a4e0c0656ee079847da4d04f981fa3200
Priyansh Maurya <65035683+pr1y4n5h@users.noreply.github.com>
2021-06-25 01:00:51 +0000

    removed unnecessary files

--- middleware/404-page.js
+++
@@ -1,5 +0,0 @@
-function routeNotFound(req, res, next) {
-  res.status(404).json({success: false, message:"Couldn't find the Route"})
-}
-
-module.exports = {routeNotFound}
--- middleware/api-server-errors.js
+++
@@ -1,6 +0,0 @@
-function apiServerErrors(err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json({success: false, message: "Some error occured", errMessage: err.message})
-}
-
-module.exports = {apiServerErrors}
